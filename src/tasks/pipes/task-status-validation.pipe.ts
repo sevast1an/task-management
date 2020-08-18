6d0193a84d922a17dac57732ef2e36cda80a2bb8
Sevastian Sergiu <sevastian.sergiu@terranet.md>
2020-08-18 15:08:03 +0300

    firs project upload

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,25 @@
+import { PipeTransform, BadRequestException } from '@nestjs/common';
+import { TaskStatus } from '../task-status.enum';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ];
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`"${value}" is not a valid status.`);
+    }
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const idx = this.allowedStatuses.indexOf(status);
+    return idx !== -1;
+  }
+}
